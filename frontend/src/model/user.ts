b788fd5daeaa66ca7865e93a7cbf701bb2b6c212
anderson202 <andersecng@gmail.com>
2020-10-12 21:08:49 -0400

    Refactor front and backend separation and update user card to display picture + call dummy API

---
+++ frontend/src/model/user.ts
@@ -0,0 +1,34 @@
+interface UserProfile {
+    playerName: string,
+    summonerId: number,
+    profileImageSrc: string
+};
+
+export const USERS: UserProfile[] = [
+    {
+        playerName: "Sec",
+        summonerId: 31390656,
+        profileImageSrc: "../src/static/images/profile/anderson.jpg"
+    },
+    {
+        playerName: "KnightHulk",
+        summonerId: 34769851,
+        profileImageSrc: "../src/static/images/profile/andrew.jpg"
+    },
+    {
+        playerName: "Asmir9990",
+        summonerId: 28676086,
+        profileImageSrc: "../src/static/images/profile/asmir.jpg"
+    },
+    {
+        playerName: "Crendez",
+        summonerId: 23815851,
+        profileImageSrc: "../src/static/images/profile/crendall.jpg"
+    },
+    {
+        playerName: "Bluegoldfish",
+        summonerId: 21183068,
+        profileImageSrc: "../src/static/images/profile/filip.jpg"
+    },
+
+];
