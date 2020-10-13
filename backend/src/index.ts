7f395ecaa8f6024f06f5c8297475a49d678ae603
teandrew <andrew.te@mail.utoronto.ca>
2020-10-12 20:13:05 -0400

    setup linting.....again

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,45 +1,48 @@
-import express from "express";
+import express from 'express';
 const app = express();
 const port = 8080; // default port to listen
 import axios from 'axios';
 import request from 'request';
-import { Accounts } from "./enum/accounts";
+import { Accounts } from './enum/accounts';
 
-const baseURL = "https://na1.api.riotgames.com/";
+const baseURL = 'https://na1.api.riotgames.com/';
 const apiKey = 'RGAPI-71775a09-8b6e-4b31-ac79-e6979f802ecf';
+
+app.get('/', (req, res) => {
+    res.send('Hello I got the home for you');
+});
 // define a route handler for the default home page
-app.get( "/api/matches/:id",  ( req, res ) => {
+app.get('/api/matches/:id', (req, res) => {
+    request(`${baseURL}lol/match/v4/matches/${req.params.id}?api_key=${apiKey}`, (error, response, body) => {
+        if (!error && response.statusCode === 200) {
+            res.send(body);
+        } else {
+            res.status(400).json({ msg: error });
+        }
+    });
+});
 
-        request(`${baseURL}lol/match/v4/matches/${req.params.id}?api_key=${apiKey}`,
-            (error, response, body) => {
-            if (!error && response.statusCode === 200) {
-                res.send( body );
-            } else {
-                res.status(400).json({msg:error})
-            }
-        });
-} );
-
-
-app.get( "/api/matchlists/by-account/:account",  ( req, res ) => {
-    if(req.params.account in Accounts) {
+app.get('/api/matchlists/by-account/:account', (req, res) => {
+    if (req.params.account in Accounts) {
         const account = (Accounts as any)[req.params.account];
-        request(`${baseURL}lol/match/v4/matchlists/by-account/${account}?api_key=${apiKey}`,
+        request(
+            `${baseURL}lol/match/v4/matchlists/by-account/${account}?api_key=${apiKey}`,
             (error, response, body) => {
-            if (!error && response.statusCode === 200) {
-                res.send( body );
-            } else {
-                res.status(400).json({msg:error})
-            }
-        });
+                if (!error && response.statusCode === 200) {
+                    res.send(body);
+                } else {
+                    res.status(400).json({ msg: error });
+                }
+            },
+        );
     } else {
-        res.status(403).json({msg:"invalid account"})
+        res.status(403).json({ msg: 'invalid account' });
     }
-} );
+});
 
-const fillGamesMap = (matches: any, gamesMap: object) =>{
-    for( const match of matches) {
-        if(! (gamesMap as any)[match.gameId]) {
+const fillGamesMap = (matches: any, gamesMap: any) => {
+    for (const match of matches) {
+        if (!(gamesMap as any)[match.gameId]) {
             (gamesMap as any)[match.gameId] = 1;
         } else {
             const val = (gamesMap as any)[match.gameId] + 1;
@@ -47,9 +50,9 @@ const fillGamesMap = (matches: any, gamesMap: object) =>{
         }
     }
     return gamesMap;
-}
+};
 
-app.get( "/api/matches",  ( req, res ) => {
+app.get('/api/matches', (req, res) => {
     let matches = {};
     const requestsArray = [
         `https://na1.api.riotgames.com/lol/match/v4/matchlists/by-account/${Accounts.Asmir9990}?queue=400&api_key=${apiKey}`,
@@ -58,20 +61,26 @@ app.get( "/api/matches",  ( req, res ) => {
         `https://na1.api.riotgames.com/lol/match/v4/matchlists/by-account/${Accounts.Crendez}?queue=400&api_key=${apiKey}`,
         `https://na1.api.riotgames.com/lol/match/v4/matchlists/by-account/${Accounts.Bluegoldfish}?queue=400&api_key=${apiKey}`,
         `https://na1.api.riotgames.com/lol/match/v4/matchlists/by-account/${Accounts.JRal}?queue=400&api_key=${apiKey}`,
-    ]
-    Promise.all(requestsArray.map((quest) => {
-        return axios.get(quest).then((response) => {
-            return response.data;
-        }).then((data) => {
-            return data;
+    ];
+    Promise.all(
+        requestsArray.map((quest) => {
+            return axios
+                .get(quest)
+                .then((response) => {
+                    return response.data;
+                })
+                .then((data) => {
+                    return data;
+                });
+        }),
+    )
+        .then((data) => {
+            data.map((adata) => {
+                matches = fillGamesMap(adata.matches, matches);
+            });
+            res.send({ data: Object.keys(matches).filter((key) => (matches as any)[key] >= 5) });
         })
-    })).then((data) => {
-        data.map((adata) => {
-            matches = fillGamesMap(adata.matches,matches);
-        });
-        res.send({data: Object.keys(matches).filter((key) => (matches as any)[key] >= 5)});
-    }).catch(() => res.status(400).json({msg: "error"}));
-
+        .catch(() => res.status(400).json({ msg: 'error' }));
 
     // request(`https://na1.api.riotgames.com/lol/match/v4/matchlists/by-account/${Accounts.Asmir9990}?queue=450&api_key=${apiKey}`,
     //         (error, response, body) => {
@@ -82,12 +91,10 @@ app.get( "/api/matches",  ( req, res ) => {
     //             res.status(400).json({msg:error})
     //         }
     //     });
-
-
-} );
+});
 
 // start the Express server
-app.listen( port, () => {
+app.listen(port, () => {
     // tslint:disable-next-line:no-console
-    console.log( `server started at http://localhost:${ port }` );
-} );
+    console.log(`server started at http://localhost:${port}`);
+});
